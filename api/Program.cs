8db2818d02e8c05f74eedad40464b69f8122c389
josh.barlow <jalapenojosh@gmail.com>
2022-09-21 11:23:32 -0600

    Made it amazon for now

--- api/Program.cs
+++ api/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<InstantRunoffContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("local")));
+builder.Services.AddDbContext<InstantRunoffContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("amazon")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
